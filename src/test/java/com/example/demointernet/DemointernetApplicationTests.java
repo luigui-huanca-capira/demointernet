48459678bea34d963f6a95c841b35655df12166a
chenaoh <lhuancac@ulasalle.edu.pe>
2026-06-26 17:11:25 -0500

    inicio

---
+++ src/test/java/com/example/demointernet/DemointernetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demointernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemointernetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
